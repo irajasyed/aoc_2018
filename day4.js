b767d91f4643675a1e1ba5c6352ded3ff79d7dd1
rajasyedabuthahir.j <rajasyedabuthahir.j@payoda.com>
2018-12-31 19:17:07 +0530

    day3 part 1 setyp

---
+++ day4.js
@@ -0,0 +1,84 @@
+/*
+TODO:
+    SETUP:
+    - parse data
+    - order log data - chronological
+    - ID key in hash table
+    - value as minutes array (index => mins, value => count of mins on all days)
+        - sleeping minutes will be incremented at each log traversal
+
+    PART 1:
+    - count all values in array - find max with ID.
+    - with found ID, get index with max value.
+    - multiply index with ID.
+*/
+let fs = require('fs')
+class TimeSheet {
+  constructor () {
+    this.guards = {} // Guards Info key - ID, value - Midnight minutes Array
+    this.inputData = []
+  }
+  /**
+   * @function getParsedData
+   * @return {object} Array of Parsed Data
+   */
+  getParsedData (logs) {
+    let re = /\[(.+)\].*#?(\d+)?/ // TODO update REGEX
+    let guardID = ''
+    return logs.map(logLine => {
+      return [<date>, #ID, type(0/1/2)]
+    })
+  }
+  /**
+   * @function getOrderedData
+   * Order by Timestamp ASC.
+   * @return {object} Array of Ordered Data
+   */
+  getOrderedData () {
+
+  }
+  /**
+   * @function preparedData
+   * Convert File Logs to readable formatted Array
+   * @return {object} Array of formatted Data
+   */
+  preparedData () {
+    fs.readFile('./input.txt', 'utf8', (err, data) => {
+      if (!err) {
+        let logLines = data.split('\n')
+        let parsedLines = this.getParsedData(logLines)
+        this.inputData = this.getOrderedData(parsedLines)
+      }
+    })
+  }
+  /**
+   * @function accumulateData
+   * @return {type} {description}
+   */
+  accumulateData () {
+
+  }
+  /**
+   * @function getMostCommonMinute
+   * @return {number} index of minutes array
+   */
+  getMostCommonMinute () {
+
+  }
+  /**
+   * @function findGuardMultipliedWithMinutes
+   * @return {number} Guard ID multiplied with Minute
+   */
+  findGuardMultipliedWithMinutes () {
+
+  }
+
+}
+
+function run () {
+  let guards = new TimeSheet()
+  guards.preparedData()
+  let answer = guards.findGuardMultipliedWithMinutes()
+  console.log('Answer:', answer)
+}
+run()
