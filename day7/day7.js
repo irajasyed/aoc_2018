802e4160f482e48228a1f5e0e4129affaa64b9af
Raja Syed Abuthahir <rajasyedab@gmail.com>
2019-01-20 02:58:38 +0530

    day7

--- day7/day7.js
+++ day7/day7.js
@@ -1,3 +1,5 @@
+// part 1 works for both input
+// working for sample input on part 2
 const fs = require('fs')
 
 function buildGraph (data) {
@@ -12,6 +14,9 @@ function buildGraph (data) {
     }
     if (!adjacencyList[nodes[1]]) {
       adjacencyList[nodes[1]] = []
+    }
+    if (!incomingEdges[nodes[0]]) {
+      incomingEdges[nodes[0]] = 0
     }
     incomingEdges[nodes[1]] = setIncomingEdges(nodes[1], incomingEdges)
   })
@@ -59,11 +64,78 @@ function doBFSTraversal (graphData) {
   return output.join('')
 }
 
+let t = 0;
+let EV = [];
+let Q = [];
+
+function part2({ adjacencyList, incomingEdges}) {
+  console.log(adjacencyList)
+  incomingEdges = Object.assign(incomingEdges)
+  let nodes = Object.keys(adjacencyList)
+  for (let node of nodes) { // sorting to maintain alphabetical order
+    adjacencyList[node] = adjacencyList[node].sort()
+  }
+  for (let node of nodes) {
+    if (incomingEdges[node] === 0) {
+      addTask(node)
+    }
+  }
+  start_work()
+  while(EV.length || Q.length) {
+    let min = EV[0]
+    for (let item of EV) {
+      if (min[0] > item[0]) {
+        min = item
+      } else if (min[0] === item[0] && min[1] > item[1]) {
+        min = item
+      }
+    }
+    t = min[0];
+    let x = min[1];
+    console.log(t, x)
+    EV = EV.filter(item =>  {
+      return item[0] !== t && item[1] !== x
+    })
+    for (let node of adjacencyList[x]) {
+      incomingEdges[node] -= 1
+      if (incomingEdges[node] === 0) {
+        addTask(node)
+      }
+    }
+    start_work()
+  }
+
+  return t
+}
+
+function addTask(x) {
+  Q.push(x)
+}
+
+function start_work () {
+  while (EV.length < 2 && Q.length) {
+    //  let xx = Q.sort()[0]
+     let min = Q[0]
+     for (let qItem of Q) {
+       if (min > qItem) {
+         min = qItem
+       }
+     }
+     let xx = min
+     Q = Q.filter(item => {
+       return item !== xx
+     })
+     console.log('Starting ' + xx + ' at ' + t)
+     EV.push([t + 1 + (xx.charCodeAt() - 'A'.charCodeAt()), xx])
+  }
+}
+
 function getOrderOfSteps () {
   fs.readFile('./input.txt', 'utf8', (err, data) => {
       let graphData = buildGraph(data)
-      let orderOfSteps = doBFSTraversal(graphData)
-      console.log('Part 1:', orderOfSteps)
+      // let orderOfSteps = doBFSTraversal(graphData)
+      // console.log('Part 1:', orderOfSteps)
+      console.log('Part 2:', part2(graphData))
   })
 }
 getOrderOfSteps()
